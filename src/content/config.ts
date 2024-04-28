ea292af2162bafa3f6009d7f831841d579615432
ryomak <ryooomaaa0413@gmail.com>
2024-04-28 20:18:38 +0900

    astro

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,5 +1,4 @@
 import { defineCollection, z } from 'astro:content'
-import {FeedItem} from "@/types/config";
 
 const postsCollection = defineCollection({
   schema: z.object({
@@ -15,10 +14,22 @@ const postsCollection = defineCollection({
   }),
 })
 
-
-
+const feedCollection = defineCollection({
+  type: 'data',
+  schema: z.object({
+    title: z.string(),
+    published: z.string().transform((str) => new Date(str)),
+    draft: z.boolean().optional(),
+    description: z.string().optional(),
+    image: z.string().optional(),
+    tags: z.array(z.string()).optional(),
+    category: z.string().optional(),
+    external: z.boolean().optional(),
+    link: z.string().optional(),
+  }),
+})
 
 export const collections = {
   posts: postsCollection,
-  feed: postsCollection,
+  feed: feedCollection,
 }
